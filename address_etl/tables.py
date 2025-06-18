bdd845a8fd5ca8013963771add78b4ccd745b313
Edmond Chuc <edmond@kurrawong.ai>
2025-06-18 21:32:57 +1000

    chore: add logging to create_metadata_table

--- address_etl/tables.py
+++ address_etl/tables.py
@@ -1,4 +1,7 @@
 import sqlite3
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def create_metadata_table(cursor: sqlite3.Cursor):
@@ -6,6 +9,7 @@ def create_metadata_table(cursor: sqlite3.Cursor):
 
     The metadata table is used to store the metadata for the ETL run.
     """
+    logger.info("Creating metadata table")
     cursor.execute(
         """
         CREATE TABLE metadata (
