763be6840278471c961035b0e2ac4aa6cb03474a
Edmond Chuc <edmond@kurrawong.ai>
2025-06-20 14:43:04 +1000

    test: fix test with new address iri query

--- tests/test_get_address_iris.py
+++ tests/test_get_address_iris.py
@@ -8,12 +8,25 @@ def test_get_address_iris_query_limit_10():
     query_limit_10 = dedent(
         """
         PREFIX addr: <https://linked.data.gov.au/def/addr/>
-        SELECT ?iri
+        PREFIX lc: <https://linked.data.gov.au/def/lifecycle/>
+        PREFIX sdo: <https://schema.org/>
+        PREFIX time: <http://www.w3.org/2006/time#>
+
+        SELECT ?iri (MAX(?_start_time) AS ?start_time)
         WHERE {
             GRAPH <urn:qali:graph:addresses> {
-                ?iri a addr:Address
+                ?iri a addr:Address ;
+                    lc:hasLifecycleStage ?lifecycle_stage .
+
+                ?lifecycle_stage sdo:additionalType <https://linked.data.gov.au/def/lifecycle-stage-types/current> ;
+                    time:hasBeginning/time:inXSDDateTime ?_start_time
+
+                FILTER NOT EXISTS {
+                    ?lifecycle_stage time:hasEnd ?end_time
+                }
             }
         }
+        GROUP BY ?iri
 
         LIMIT 10
         """
@@ -26,12 +39,25 @@ def test_get_address_iris_query_no_limit():
     query_no_limit = dedent(
         """
         PREFIX addr: <https://linked.data.gov.au/def/addr/>
-        SELECT ?iri
+        PREFIX lc: <https://linked.data.gov.au/def/lifecycle/>
+        PREFIX sdo: <https://schema.org/>
+        PREFIX time: <http://www.w3.org/2006/time#>
+
+        SELECT ?iri (MAX(?_start_time) AS ?start_time)
         WHERE {
             GRAPH <urn:qali:graph:addresses> {
-                ?iri a addr:Address
+                ?iri a addr:Address ;
+                    lc:hasLifecycleStage ?lifecycle_stage .
+
+                ?lifecycle_stage sdo:additionalType <https://linked.data.gov.au/def/lifecycle-stage-types/current> ;
+                    time:hasBeginning/time:inXSDDateTime ?_start_time
+
+                FILTER NOT EXISTS {
+                    ?lifecycle_stage time:hasEnd ?end_time
+                }
             }
         }
+        GROUP BY ?iri
     """
     ).strip()
     assert query == query_no_limit
